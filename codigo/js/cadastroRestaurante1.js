bb07c45fb437c10ce14fa40a5db1979694a08650
Camilla Teodoro <camillateodoro@hotmail.com>
2022-07-03 16:00:47 -0300

    verificação senhas iguais no cadastro fornecedor

--- codigo/js/cadastroRestaurante1.js
+++ codigo/js/cadastroRestaurante1.js
@@ -1,8 +1,14 @@
-function register(){
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("#password").value;
-    sessionStorage.setItem('email', email);
-    sessionStorage.setItem('password', password);
+function register() {
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+  let password2 = document.querySelector("#password2").value;
+
+  if (password2 == password) {
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("password", password);
+  } else {
+    alert("Senhas digitadas são diferentes.");
+  }
 }
 
-document.querySelector("#button").addEventListener('click', register);
+document.querySelector("#button").addEventListener("click", register);
