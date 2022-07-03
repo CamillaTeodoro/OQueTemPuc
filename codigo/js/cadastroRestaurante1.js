dd11957e754edbff3590170818a4f740bf7e96de
Camilla Teodoro <camillateodoro@hotmail.com>
2022-07-03 16:05:11 -0300

    removida verificação de senha

--- codigo/js/cadastroRestaurante1.js
+++ codigo/js/cadastroRestaurante1.js
@@ -1,14 +1,8 @@
 function register() {
   let email = document.querySelector("#email").value;
   let password = document.querySelector("#password").value;
-  let password2 = document.querySelector("#password2").value;
-
-  if (password2 == password) {
-    sessionStorage.setItem("email", email);
-    sessionStorage.setItem("password", password);
-  } else {
-    alert("Senhas digitadas são diferentes.");
-  }
+  sessionStorage.setItem("email", email);
+  sessionStorage.setItem("password", password);
 }
 
 document.querySelector("#button").addEventListener("click", register);
