5abf80f62f28fe86b223802e22f08ee9e0b26f28
Camilla Teodoro <camillateodoro@hotmail.com>
2022-07-03 15:44:57 -0300

    confirmação de senhas iguais cadastro cliente

--- codigo/js/cadastrocliente.js
+++ codigo/js/cadastrocliente.js
@@ -1,16 +1,25 @@
 const LOGIN_URL = "login.html";
 
-function registerPeople(){
-    let data = readPeople();
-    let email = document.querySelector("#email-people").value;
-    let password = document.querySelector("#password-people").value;
-    let id = generateUUID();
-    
+function registerPeople() {
+  let data = readPeople();
+  let email = document.querySelector("#email-people").value;
+  let password = document.querySelector("#password-people").value;
+  let password2 = document.querySelector("#password-people2").value;
+  let id = generateUUID();
+
+  if (password2 == password) {
     // create object and save
-    let people_object = { 'id': generateUUID(), 'email': email,'password': password }
+    let people_object = {
+      id: generateUUID(),
+      email: email,
+      password: password,
+    };
     data.people.push(people_object);
-    saveData_2(data);   
+    saveData_2(data);
     window.location.href = LOGIN_URL;
+  } else {
+    alert("As senhas não estão iguais.");
+  }
 }
 
-document.getElementById('cadastro').addEventListener('click', registerPeople);
+document.getElementById("cadastro").addEventListener("click", registerPeople);
